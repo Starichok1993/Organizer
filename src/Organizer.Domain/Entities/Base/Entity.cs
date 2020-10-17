47682417805591d1f8fa3a1fa9c33305a5041ad5
Alex Star <star@n1.by>
2020-10-17 16:39:04 +0300

    add base entities. (code doesn't work) (do not use)

---
+++ src/Organizer.Domain/Entities/Base/Entity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Organizer.Domain.Entities.Base
+{
+
+    public interface IEntity
+    {
+        object[] GetKeys();
+
+    }
+
+    public interface IEntity <TKey> : IEntity
+    {
+        TKey Id { get; }
+
+    }
+
+    public abstract class Entity<TKey> : IEntity<TKey>
+    {
+        public virtual TKey Id { get; protected set; }
+
+        protected Entity()
+        { 
+        }
+
+        protected Entity(TKey id)
+        {
+            Id = id;
+        }
+
+        public virtual object[] GetKeys()
+        {
+            return new object[] { Id };
+        }
+                              
+    }
+
+    public abstract class Entity : Entity<int>
+    {
+        
+    }
+}
---
+++ src/Organizer.Domain/Entities/DbVersion.cs
@@ -0,0 +1,19 @@
+﻿using Organizer.Domain.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Organizer.Domain.Entities
+{
+    public class DbVersion : Entity
+    {
+        public string Version { get; private set; }
+        public string Description { get; private set; }
+        public DateTime DateApplied { get; private set; }
+
+        protected DbVersion()
+        {
+            
+        }
+    }
+}
